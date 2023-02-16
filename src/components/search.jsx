7b32039e72c4f98c68411450fce724dd887cb937
adamdgit <adam_d@outlook.com>
2023-02-17 01:51:11 +1100

    Added search input to search artists

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,19 +1,44 @@
 import { createSignal, createEffect, For } from "solid-js"
 import "./search.css"
+import { searchSongs } from "../api/search"
 
 export default function Search(props) {
 
-  const [playlists, setPlaylists] = createSignal()
+  // destructure props, token prop passed as function getter 
+  // is destructured as a variable
+  const { token, username, userID } = props;
+  const [searchQuery, setSearchQuery] = createSignal();
+  const [searchResults, setSearchResults] = createSignal();
 
-  
+  async function handleSearch() {
+    const { errorMsg, searchResult } = await searchSongs(token, searchQuery())
+    console.log(searchResult.artists.items)
+    setSearchResults(searchResult.artists.items);
+  }
 
   return (
     <div class="library-container">
-      <h1>Hello: {props.username}</h1>
-      <h2>Playlists:</h2>
-      <ul class="playlists">
+      <h1>Welcome: {props.username}</h1>
+      <h2>Test your music knowledge</h2>
+      <p>search and select an artist to play</p>
+      <p>
+        You will be given a random song preview to listen to, 
+        try and guess which album out of 4 choices the song is from.
+        Earn points for every correct guess.
+      </p>
+      <input type="text" onChange={(e) => setSearchQuery(e.target.value)} />
+      <button onClick={() => handleSearch()}>Search for an Artist</button>
+      
+      <div class="search-results">
+      <For each={searchResults()}>
+        {(item, index) =>
+          <div>
+            <img src={item.images[1].url} width={160} height={160} />
+            {item.name}
+          </div>}
+      </For>
+      </div>
 
-      </ul>
     </div>
   )
 }
