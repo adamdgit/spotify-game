02fac982db2b53e7fe8c2510eaa47d659cb0e701
adamdgit <adam_d@outlook.com>
2023-02-17 01:50:43 +1100

    Added search artists API function

---
+++ src/api/search.js
@@ -0,0 +1,24 @@
+import axios from "axios"
+
+export async function searchSongs(token, query) {
+
+  let errorMsg = false
+  let searchResult = []
+
+  await axios.get('https://api.spotify.com/v1/search', {
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    params: {
+      q: query,
+      type: 'artist',
+      limit: 5
+    }
+  }).then((res) => {
+    searchResult = res.data
+  }).catch(error => console.error(error))
+
+  return { errorMsg, searchResult }
+}
